6a900ef60fab399a6d88c1ac7fc858c63518c84e
mehul <mehul@veravalonline.com>
2024-10-13 15:53:29 +0530

    admin

--- app/Http/Controllers/admin/AdminAuthController.php
+++ app/Http/Controllers/admin/AdminAuthController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\admin\admin;
+use App\Models\admin\Admin;
 use Illuminate\Support\Facades\Hash;
 
 
